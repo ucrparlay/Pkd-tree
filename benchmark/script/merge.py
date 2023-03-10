f08717d5a7257d926b35c9ab4a2f12edc229027e
RomaLzhih <791191656@qq.com>
2023-03-10 03:05:15 -0800

    update cgal

--- benchmark/script/merge.py
+++ benchmark/script/merge.py
@@ -9,22 +9,42 @@ from unittest import case
 import math
 import csv
 
-path = "benchmark"
-subpath = "craft_var_dim"
-Nodes = [100000]
-Dims = [2,3,5,7,9,10,12,15]
-header = [subpath,'nodes','dims','file','build_time','query_time']
-csvFilePointer = open(path+'/' +'craft'+'/'+'summary.csv',"w",newline='')
-csvFilePointer.truncate()
-csvWriter=csv.writer(csvFilePointer)
-csvWriter.writerow(header)
+solverName = ['my_kd', 'cgal']
+resMap = {'my_kd': 'res.out', 'cgal': 'cgal_res.out'}
 
-for node in Nodes:
+path = "benchmark"
+benchDim = "craft_var_dim"
+benchNode = "craft_var_node"
+Nodes = [10000, 50000, 100000, 500000, 800000, 1000000, 2000000]
+Dims = [2, 3, 5, 7, 9, 10, 12, 15]
+header = ['solver', 'benchType', 'nodes',
+          'dims', 'file', 'buildTime', 'queryTime']
+
+for solver in solverName:
+    csvFilePointer = open(path + '/script/' +
+                          solver+'.csv', "w", newline='')
+    csvFilePointer.truncate()
+    csvWriter = csv.writer(csvFilePointer)
+    csvWriter.writerow(header)
+
+    node = 100000
     for dim in Dims:
-        P = path+'/'+subpath+'/'+str(node)+"_"+str(dim)+'/res.out'
-        lines=open(P,"r").readlines();
+        P = path+'/'+benchDim+'/' + \
+            str(node)+"_"+str(dim)+'/'+resMap[solver]
+        lines = open(P, "r").readlines()
         for line in lines:
             l = " ".join(line.split())
             l = l.split(' ')
-            csvWriter.writerow([subpath,node,dim,l[0],l[1],l[2]])
-            
+            csvWriter.writerow(
+                [solver, benchDim, node, dim, l[0], l[1], l[2]])
+
+    dim = 5
+    for node in Nodes:
+        P = path+'/'+benchNode+'/' + \
+            str(node)+"_"+str(dim)+'/'+resMap[solver]
+        lines = open(P, "r").readlines()
+        for line in lines:
+            l = " ".join(line.split())
+            l = l.split(' ')
+            csvWriter.writerow(
+                [solver, benchNode, node, dim, l[0], l[1], l[2]])
