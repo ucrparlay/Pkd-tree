68b345346dc8dfcfbbe033b63d92d313fb18c686
ziyang <791191656@qq.com>
2023-12-31 20:49:25 -0800

    update batch insert/delete

--- script/run_summary.sh
+++ script/run_summary.sh
@@ -4,10 +4,10 @@
 #     kill $$
 # } &
 
-Solvers=("cgal")
-# Solvers=("zdtree" "test" "cgal")
-# Node=(100000000 1000000000)
-Node=(1000000000)
+# Solvers=("cgal")
+Solvers=("zdtree" "test")
+Node=(100000000 1000000000)
+# Node=(1000000000)
 Dim=(2 3 5 9)
 # Dim=(9)
 declare -A datas
