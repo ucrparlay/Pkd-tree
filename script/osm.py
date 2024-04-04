722fb6bf66e99fd78b8022b24cc010941703a53a
RomaLzhih <791191656@qq.com>
2024-04-04 14:25:10 -0700

    udpate generate script

--- script/osm.py
+++ script/osm.py
@@ -13,17 +13,8 @@ class OSMHandler(osm.SimpleHandler):
         for tag in elem.tags:
             self.osm_data.append(
                 [
-                    elem_type,
                     elem.id,
-                    elem.version,
-                    elem.visible,
                     pd.Timestamp(elem.timestamp),
-                    elem.uid,
-                    elem.user,
-                    elem.changeset,
-                    len(elem.tags),
-                    tag.k,
-                    tag.v,
                     elem.location.lon,
                     elem.location.lat,
                 ]
@@ -32,36 +23,28 @@ class OSMHandler(osm.SimpleHandler):
     def node(self, n):
         self.tag_inventory(n, "node")
 
-    # def way(self, w):
-    #     self.tag_inventory(w, "way")
-    #
-    # def relation(self, r):
-    #     self.tag_inventory(r, "relation")
-
 
 osmhandler = OSMHandler()
 # scan the input file and fills the handler list accordingly
-osmhandler.apply_file("antarctica-latest.osm.pbf")
+osmhandler.apply_file("../benchmark/real_world/antarctica-latest.osm.pbf")
 
 # transform the list into a pandas DataFrame
 data_colnames = [
-    "type",
     "id",
-    "version",
-    "visible",
     "ts",
-    "uid",
-    "user",
-    "chgset",
-    "ntags",
-    "tagkey",
-    "tagvalue",
     "lon",
     "lat",
 ]
-df_osm = pd.DataFrame(osmhandler.osm_data, columns=data_colnames)
-# df_osm = tag_genome.sort_values(by=["type", "id", "ts"])
+df = pd.DataFrame(osmhandler.osm_data, columns=data_colnames)
+df = df.drop_duplicates()
+df["ts"] = df["ts"].dt.tz_localize(None)
+df.set_index("ts", inplace=True)
+print(df.dtypes)
 
-print(len(df_osm.index))
-print(df_osm)
-df_osm.to_csv("osm_data.csv")
+start_time = "2014-04-02 08:00:00"
+end_time = "2024-04-02 08:00:00"
+df = df.sort_index().loc[start_time:end_time, :]
+
+print(len(df.index))
+print(df.head())
+df.to_csv("osm_data.csv")
