29e632a3c60649435475378ab5edff2a26c39695
ziyang <791191656@qq.com>
2024-04-07 13:49:44 -0700

    finish remove technique

--- script/run_remove_techniques.sh
+++ script/run_remove_techniques.sh
@@ -5,8 +5,8 @@ cd "../build/" || exit
 rm -rf *
 
 run_test() {
-	varden_path="/ssd0/zmen002/kdtree/ss_varden/10000000_3/1.in"
-	uniform_path="/ssd0/zmen002/kdtree/uniform/10000000_3/1.in"
+	varden_path="/data3/zmen002/kdtree/ss_varden/1000000000_3/1.in"
+	uniform_path="/data3/zmen002/kdtree/uniform/1000000000_3/1.in"
 	PARLAY_NUM_THREADS=1 ./test -p ${uniform_path} -d 3 -t 0 -q 0 -r 2 -i 0
 	PARLAY_NUM_THREADS=192 ./test -p ${uniform_path} -d 3 -t 0 -q 0 -r 2 -i 0
 	PARLAY_NUM_THREADS=1 ./test -p ${varden_path} -d 3 -t 0 -q 0 -r 2 -i 0
