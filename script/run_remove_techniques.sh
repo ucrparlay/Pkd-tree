9ba41c7c01ee80b9c7cc5bf8a6d4ac2a224a5f58
ziyang <791191656@qq.com>
2024-04-07 20:16:17 -0700

    update remove technique

--- script/run_remove_techniques.sh
+++ script/run_remove_techniques.sh
@@ -14,12 +14,12 @@ run_test() {
 }
 
 run_cache() {
-	varden_path="/data3/zmen002/kdtree/ss_varden/1000000000_3/1.in"
-	uniform_path="/data3/zmen002/kdtree/uniform/1000000000_3/1.in"
-	perf stat -e cache-misses PARLAY_NUM_THREADS=1 ./test -p ${uniform_path} -d 3 -t 0 -q 0 -r 2 -i 0
-	perf stat -e cache-misses PARLAY_NUM_THREADS=192 ./test -p ${uniform_path} -d 3 -t 0 -q 0 -r 2 -i 0
-	perf stat -e cache-misses PARLAY_NUM_THREADS=1 ./test -p ${varden_path} -d 3 -t 0 -q 0 -r 2 -i 0
-	perf stat -e cache-misses PARLAY_NUM_THREADS=192 ./test -p ${varden_path} -d 3 -t 0 -q 0 -r 2 -i 0
+	varden_path="/ssd0/zmen002/kdtree/ss_varden/10000000_3/1.in"
+	uniform_path="/ssd0/zmen002/kdtree/uniform/10000000_3/1.in"
+	PARLAY_NUM_THREADS=1 perf stat -e cache-misses ./test -p ${uniform_path} -d 3 -t 0 -q 0 -r 2 -i 0
+	PARLAY_NUM_THREADS=192 perf stat -e cache-misses ./test -p ${uniform_path} -d 3 -t 0 -q 0 -r 2 -i 0
+	PARLAY_NUM_THREADS=1 perf stat -e cache-misses ./test -p ${varden_path} -d 3 -t 0 -q 0 -r 2 -i 0
+	PARLAY_NUM_THREADS=192 perf stat -e cache-misses ./test -p ${varden_path} -d 3 -t 0 -q 0 -r 2 -i 0
 }
 
 remove_technique() {
@@ -39,10 +39,10 @@ test_cache_usage() {
 }
 
 # NOTE: NTH_ELEMENT | ONE_LEVEL | SAMPLE | TEST_CACHE
-remove_technique "ON" "OFF" "ON" "OFF"
-remove_technique "ON" "ON" "ON" "OFF"
-remove_technique "OFF" "ON" "OFF" "OFF"
-remove_technique "ON" "ON" "OFF" "OFF"
+# remove_technique "ON" "OFF" "ON" "OFF"
+# remove_technique "ON" "ON" "ON" "OFF"
+# remove_technique "OFF" "ON" "OFF" "OFF"
+# remove_technique "ON" "ON" "OFF" "OFF"
 
 echo ">>>>>>>>>>>>>>>>begin cache>>>>>>>>>>>>>>>>"
 test_cache_usage "ON" "OFF" "ON" "ON"
