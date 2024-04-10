c040e4af33a881b9e9cf3881105c63983cda59fa
ziyang <791191656@qq.com>
2024-04-10 15:00:26 -0700

    finish bath insertion osm test

--- script/run.sh
+++ script/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-{
-	sleep 6h
-	sleep 45m
-	kill $$
-} &
+# {
+# 	sleep 6h
+# 	sleep 45m
+# 	kill $$
+# } &
 
-./run_inba_ratio.sh
-./scalability.sh
+PARLAY_NUM_THREADS=192 timeout 3700s numactl -i all ./../../pbbsbench_x/build/zdtree -p /data3/zmen002/kdtree/uniform/100000000_2/1.in -d 2 -t 0 -k 10 -q 2048 -i 0 -r 3 | tee ../benchmark/real_world/osm_year_zdtree.out
+
+PARLAY_NUM_THREADS=192 timeout 3700s numactl -i all ./../../pbbsbench_x/build/zdtree -p /data3/zmen002/kdtree/uniform/100000000_2/1.in -d 2 -t 0 -k 10 -q 4096 -i 0 -r 3 | tee ../benchmark/real_world/osm_month_zdtree.out
