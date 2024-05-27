47e13a7af887c9c2603123ddc652ade969e54a20
ziyang <791191656@qq.com>
2024-05-27 14:10:04 -0700

    add serial test

--- script/run.sh
+++ script/run.sh
@@ -2,22 +2,21 @@
 
 # NOTE: find the process id by script name
 # pgrep -f script_name
-
 # sleep 330m
 # ./kill_script.sh $1
 
-# PARLAY_NUM_THREADS=192 timeout 3700s numactl -i all ./../../pbbsbench_x/build/zdtree -p /data3/zmen002/kdtree/uniform/100000000_2/1.in -d 2 -t 0 -k 10 -q 2048 -i 0 -r 3 | tee ../benchmark/real_world/osm_year_zdtree.out
-#
-# PARLAY_NUM_THREADS=192 timeout 3700s numactl -i all ./../../pbbsbench_x/build/zdtree -p /data3/zmen002/kdtree/uniform/100000000_2/1.in -d 2 -t 0 -k 10 -q 4096 -i 0 -r 3 | tee ../benchmark/real_world/osm_month_zdtree.out
+# NOTE: varden insert
+# PARLAY_NUM_THREADS=192 numactl -i all ./../build/test -p /data3/zmen002/kdtree/ss_varden/1000000000_3/1.in -t 0 -s 0 -d 3 -q 8192 -r 1 -i 1 -k 10 | tee data/serialBatch_origin/Parallel_insert_varden.log
+PARLAY_NUM_THREADS=1 numactl -i all ./../build/test -p /data3/zmen002/kdtree/ss_varden/1000000000_3/1.in -t 0 -s 0 -d 3 -q 8192 -r 1 -i 1 -k 10 | tee data/serialBatch_origin/serial_insert_varden.log
 
-# PARLAY_NUM_THREADS=192 numactl -i all ./../build/test -p /data3/zmen002/kdtree/ss_varden/1000000000_3/1.in -t 0 -s 0 -d 3 -q 8192 -r 1 -i 1 -k 10 | tee data/Parallel_insert_varden.log
-# PARLAY_NUM_THREADS=1 numactl -i all ./../build/test -p /data3/zmen002/kdtree/ss_varden/1000000000_3/1.in -t 0 -s 0 -d 3 -q 8192 -r 1 -i 1 -k 10 | tee data/serial_insert_varden.log
+# NOTE: varden delete
+# PARLAY_NUM_THREADS=192 numactl -i all ./../build/test -p /data3/zmen002/kdtree/ss_varden/1000000000_3/1.in -t 0 -s 0 -d 3 -q 16384 -r 3 -i 1 -k 10 | tee data/serialBatch_origin/Parallel_delete_varden.out
+PARLAY_NUM_THREADS=1 numactl -i all ./../build/test -p /data3/zmen002/kdtree/ss_varden/1000000000_3/1.in -t 0 -s 0 -d 3 -q 16384 -r 3 -i 1 -k 10 | tee data/serialBatch_origin/serial_delete_varden.out
 
-# PARLAY_NUM_THREADS=192 numactl -i all ./../build/test -p /data3/zmen002/kdtree/ss_varden/1000000000_3/1.in -t 0 -s 0 -d 3 -q 16384 -r 3 -i 1 -k 10 | tee data/Parallel_delete_varden.out
-PARLAY_NUM_THREADS=1 numactl -i all ./../build/test -p /data3/zmen002/kdtree/ss_varden/1000000000_3/1.in -t 0 -s 0 -d 3 -q 16384 -r 3 -i 1 -k 10 | tee data/serial_delete_varden.out
+# NOTE: uniform insert
+# PARLAY_NUM_THREADS=192 numactl -i all ./../build/test -p /data3/zmen002/kdtree/uniform/1000000000_3/1.in -t 0 -s 0 -d 3 -q 8192 -r 1 -i 1 -k 10 | tee data/serialBatch_origin/Parallel_insert_uniform.log
+PARLAY_NUM_THREADS=1 numactl -i all ./../build/test -p /data3/zmen002/kdtree/uniform/1000000000_3/1.in -t 0 -s 0 -d 3 -q 8192 -r 1 -i 1 -k 10 | tee data/serialBatch_origin/serial_insert_uniform.log
 
-# PARLAY_NUM_THREADS=192 numactl -i all ./../build/test -p /data3/zmen002/kdtree/uniform/1000000000_3/1.in -t 0 -s 0 -d 3 -q 8192 -r 1 -i 1 -k 10 | tee data/Parallel_insert_uniform.log
-# PARLAY_NUM_THREADS=1 numactl -i all ./../build/test -p /data3/zmen002/kdtree/uniform/1000000000_3/1.in -t 0 -s 0 -d 3 -q 8192 -r 1 -i 1 -k 10 | tee data/serial_insert_uniform.log
-
-# PARLAY_NUM_THREADS=192 numactl -i all ./../build/test -p /data3/zmen002/kdtree/uniform/1000000000_3/1.in -t 0 -s 0 -d 3 -q 16384 -r 3 -i 1 -k 10 | tee data/Parallel_delete_uniform.out
-PARLAY_NUM_THREADS=1 numactl -i all ./../build/test -p /data3/zmen002/kdtree/uniform/1000000000_3/1.in -t 0 -s 0 -d 3 -q 16384 -r 3 -i 1 -k 10 | tee data/serial_delete_uniform.out
+# NOTE: uniform delete
+# PARLAY_NUM_THREADS=192 numactl -i all ./../build/test -p /data3/zmen002/kdtree/uniform/1000000000_3/1.in -t 0 -s 0 -d 3 -q 16384 -r 3 -i 1 -k 10 | tee data/serialBatch_origin/Parallel_delete_uniform.out
+PARLAY_NUM_THREADS=1 numactl -i all ./../build/test -p /data3/zmen002/kdtree/uniform/1000000000_3/1.in -t 0 -s 0 -d 3 -q 16384 -r 3 -i 1 -k 10 | tee data/serialBatch_origin/serial_delete_uniform.out
