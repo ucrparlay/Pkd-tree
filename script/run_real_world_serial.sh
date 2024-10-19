c9c7d951caa92f662bde2bee192c0b888b411a21
RomaLzhih <791191656@qq.com>
2024-10-18 17:16:52 -0700

    update format script

--- script/run_real_world_serial.sh
+++ script/run_real_world_serial.sh
@@ -52,7 +52,9 @@ for queryType in ${QueryTypes[@]}; do
             perf report --stdio --input=${perf_data_name} >${perf_report_name}
 
             echo -n "${filename} " >>${dest}
-            res=$(grep -E "Samples|Event count|\\b${func_name}\\b" ${perf_report_name} | awk '$1!="0.00%"' | awk '/Event count/ {print $NF} /'"${func_name}"'/ {gsub("%", "", $1); print $1}' | awk '{v[NR]=$1;} END {for(i=1;i<=NR;i+=2){if(i+1<=NR){res=(v[i]*v[i+1])/100/1000000; printf "%d\n", res;}}}')
+            res=$(grep -E "Samples|Event count|\\b${func_name}\\b" ${perf_report_name} | awk '/Event count/ {print $NF} /'"${func_name}"'/ {gsub("%", "", $1); }' | awk '{if($1>100){print; kp=1}else if(kp){print; kp=0}}'
+
+            # awk '{v[NR]=$1;} END {for(i=1;i<=NR;i+=2){if(i+1<=NR){res=(v[i]*v[i+1])/100/1000000; printf "%d\n", res;}}}')
             echo -n $res | paste -sd ' ' >>${dest}
         done
     done
